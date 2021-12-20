0d304bedcf379362e141c6df60933efec03853cd
Rosyid Ridho <rosyidgrobogan@gmail.com>
2021-12-21 05:01:01 +0700

    project init

---
+++ src/main/java/com/rosyidgrobogan/moviestoreapitask/MovieStoreApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.rosyidgrobogan.moviestoreapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStoreApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieStoreApiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosyidgrobogan/moviestoreapitask/MovieStoreApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosyidgrobogan.moviestoreapitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieStoreApiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
